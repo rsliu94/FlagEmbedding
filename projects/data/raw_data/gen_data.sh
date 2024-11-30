31d470256c68f25c82f2e4d977f395f0fe0f7340
Runsheng Liu <rsliu94@gmail.com>
2024-12-01 05:02:16 +0800

    update

--- projects/data/raw_data/gen_data.sh
+++ projects/data/raw_data/gen_data.sh
@@ -2,5 +2,4 @@
 # validation data do not modify original dataset, just split the train.csv into train/val.csv under different method:
 # v1: random select 33.3% QuestionId from train.csv as validation set
 # v2: random select 33.3% QuestionId [with no NaN MisconceptionId] from train.csv as validation set
-mkdir validation_v1 validation_v2
 python prepare_val_data.py
