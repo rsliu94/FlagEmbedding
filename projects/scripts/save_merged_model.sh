271a93d18b8729ed412fd05b4ba5eb371e87e2e6
Runsheng Liu <rsliu94@gmail.com>
2024-12-12 20:52:46 +0800

    clean

--- projects/scripts/save_merged_model.sh
+++
@@ -1,4 +0,0 @@
-python save_merged_model.py \
-    --base_model_path BAAI/bge-en-icl \
-    --lora_path ../model_output/icl_hn_finetune_round1/lora_epoch_3 \
-    --output_dir ../model_output/icl_hn_finetune_round1/merged_model_lora_epoch_3
